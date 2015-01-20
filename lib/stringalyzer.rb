c851299f734a147ae5944015c348ce553a5d227d
Dan Buettner <dan@danbuettner.net>
2015-01-20 11:10:44 -0600

    contains and refactoring

--- lib/stringalyzer.rb
+++ lib/stringalyzer.rb
@@ -1,11 +1,18 @@
 class Stringalyzer
 
-  def self.name_prefix?(str)
-    !!str.match(/\A(Mr|Mrs|Miss|Dr)/)
+  def initialize(string)
+    @string = string
   end
 
-  def self.name_suffix?(str)
-    !!str.match(/(Jr|Sr|III)\Z/)
-   end
+  def name_prefix?
+    !!@string.match(/\A(Mr|Mrs|Miss|Dr)/)
+  end
 
+  def name_suffix?
+    !!@string.match(/(Jr|Sr|III)\Z/)
+  end
+
+  def contains?(str)
+    !!@string.match(/#{str}/)
+  end
 end
--- spec/stringalyzer_spec.rb
+++ spec/stringalyzer_spec.rb
@@ -5,27 +5,27 @@ RSpec.describe Stringalyzer do
   describe '.name_prefix?' do
     it 'is true for Mr' do
       str = "Mr Oliver Johnson"
-      expect(Stringalyzer.name_prefix?(str)).to eq true
+      expect(Stringalyzer.new(str).name_prefix?).to eq true
     end
 
     it 'is true for Mrs' do
       str = "Mrs Olivia Jones"
-      expect(Stringalyzer.name_prefix?(str)).to eq true
+      expect(Stringalyzer.new(str).name_prefix?).to eq true
     end
 
     it 'is true for Miss' do
       str = "Miss Olivia Jones"
-      expect(Stringalyzer.name_prefix?(str)).to eq true
+      expect(Stringalyzer.new(str).name_prefix?).to eq true
     end
 
     it 'is true for Dr' do
       str = "Dr Olivia Jones"
-      expect(Stringalyzer.name_prefix?(str)).to eq true
+      expect(Stringalyzer.new(str).name_prefix?).to eq true
     end
 
     it 'is false for a name' do
       str = "Olivia Jones"
-      expect(Stringalyzer.name_prefix?(str)).to eq false
+      expect(Stringalyzer.new(str).name_prefix?).to eq false
     end
   end
 
@@ -33,22 +33,29 @@ RSpec.describe Stringalyzer do
   describe ".name_suffix?" do
     it 'is true for Jr' do
       str = "Oliver Johnson Jr"
-      expect(Stringalyzer.name_suffix?(str)).to eq true
+      expect(Stringalyzer.new(str).name_suffix?).to eq true
     end
 
     it 'is true for Sr' do
       str = "Oliver Johnson Sr"
-      expect(Stringalyzer.name_suffix?(str)).to eq true
+      expect(Stringalyzer.new(str).name_suffix?).to eq true
     end
 
     it 'is true for III' do
       str = "Oliver Johnson III"
-      expect(Stringalyzer.name_suffix?(str)).to eq true
+      expect(Stringalyzer.new(str).name_suffix?).to eq true
     end
 
     it 'is true for a name' do
       str = "Oliver Johnson"
-      expect(Stringalyzer.name_suffix?(str)).to eq false
+      expect(Stringalyzer.new(str).name_suffix?).to eq false
+    end
+  end
+
+  describe ".contains?" do
+    it "is true when it does" do
+      str = "Olivia Jones loves fishing"
+      expect(Stringalyzer.new(str).contains?('Jones')).to eq true
     end
   end
 
